3bdd4b6be356eacd6b9894a67dc685f7cc51cacf
조하운 <79410456+henrynoowah@users.noreply.github.com>
2022-10-22 18:28:54 +0900

    yml config nojekyll

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  enabled: process.env.NODE_ENV === "production",
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
