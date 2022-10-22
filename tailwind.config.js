56435a7c0f5234c775030ee24bd50444b3bcefa9
조하운 <79410456+henrynoowah@users.noreply.github.com>
2022-10-22 17:54:52 +0900

    tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  enabled: process.env.NODE_ENV === "production",
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
