2025c5a7648c3edf6b97eff5766c6234f10fed72
조하운 <79410456+henrynoowah@users.noreply.github.com>
2022-10-30 00:22:48 +0900

    add: layouts

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,8 +1,17 @@
 import { NextPage } from 'next'
 import React from 'react'
+import Layouts from '../../layouts/Layouts'
 
 const index: NextPage = () => {
-  return <div>index</div>
+  return (
+    <Layouts header>
+      <div className="w-full h-full flext justify-center items-center bg-dark">
+        <div className="w-full h-full flex justify-center items-center">
+          <h1 className="text-primary text-[36px]">Posts</h1>
+        </div>
+      </div>
+    </Layouts>
+  )
 }
 
 export default index
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        inherit: 'inherit',
+        transparent: 'transparent',
+        primary: {
+          DEFAULT: '#6A9286',
+        },
+        secondary: {
+          DEFAULT: '#4A6F76'
+        },
+        dark: {
+          DEFAULT: '#090E19'
+        },
+        light: {
+          DEFAULT: "#E9E6DF"
+        }
+      }
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 }
