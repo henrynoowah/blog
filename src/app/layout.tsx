ee116629c4d7f48530396e61bfa1443f57d5176d
조하운 <79410456+henrynoowah@users.noreply.github.com>
2024-01-22 22:45:12 +0900

    feat: custom favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,12 @@ interface Params {
 const RootLayout = ({ children }: Params) => {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body suppressHydrationWarning={true}>{children}</body>
     </html>
   )
