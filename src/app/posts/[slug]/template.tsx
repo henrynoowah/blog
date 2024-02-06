38776ae0274a666a8a6b2b8de326c30acdc7d4ce
조하운 <79410456+henrynoowah@users.noreply.github.com>
2024-02-07 00:53:18 +0900

    fix: overflow on framer template

--- src/app/posts/[slug]/template.tsx
+++ src/app/posts/[slug]/template.tsx
@@ -10,6 +10,7 @@ interface Params {
 const PostTemplate = ({ children }: Params) => {
   return (
     <motion.div
+      className="w-full"
       initial={{ y: -10, opacity: 0 }}
       animate={{ y: 0, opacity: 100 }}
       transition={{ ease: 'easeInOut', duration: 0.5 }}
