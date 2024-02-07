b136f3dd54b5c5306c1d123ed671098d575bf2a5
조하운 <79410456+henrynoowah@users.noreply.github.com>
2024-02-07 21:54:46 +0900

    style: mobile navigation update

--- src/components/common/Header/Nav_mobile.tsx
+++ src/components/common/Header/Nav_mobile.tsx
@@ -18,9 +18,9 @@ const Nav_mobile = ({ navOption }: Params) => {
       <Button_nav onChange={setIsNavOpened} />
 
       <nav
-        className={`absolute left-0 top-[72px] border-t border-t-1 border-light/20 border-solid ${
-          isNavOpened ? 'translate-x-0' : 'translate-x-[100%]'
-        } transition duration-300 ease-out bg-primary text-light w-full h-[100vh] p-4 
+        className={`absolute left-0 top-[72px] bg-dark/80 ${
+          isNavOpened ? 'translate-x-0 opacity-100 pointer-events-auto' : 'translate-x-0 opacity-0 pointer-events-none'
+        } transition duration-300 ease-out text-light w-full h-[100vh] 
         `}
       >
         <ul className="flex flex-col text-lg font-semibold">
@@ -30,7 +30,9 @@ const Nav_mobile = ({ navOption }: Params) => {
               style={{
                 transitionDuration: `${index * 350}ms`
               }}
-              className={`transition ${isNavOpened ? 'translate-x-0' : 'translate-x-[200%] !duration-0'}`}
+              className={`transition bg-primary px-4 py-1 first:border-t border-t-1 border-light/20 border-solid last:pb-2 last:rounded-bl-xl ${
+                isNavOpened ? 'translate-x-0' : 'translate-x-[200%]'
+              }`}
             >
               <Link href={nav.href}>
                 <Button className="w-full flex justify-between items-center bg-transparent !text-light !text-md py-2 text-left">
