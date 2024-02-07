4627e8c37f35b97e2a84d1716678cd845a0d2d0d
조하운 <79410456+henrynoowah@users.noreply.github.com>
2024-02-07 12:05:11 +0900

    style: deskptop layout fix

--- src/app/posts/[slug]/template.tsx
+++ src/app/posts/[slug]/template.tsx
@@ -10,7 +10,7 @@ interface Params {
 const PostTemplate = ({ children }: Params) => {
   return (
     <motion.div
-      className="w-full"
+      className="w-full max-w-2xl"
       initial={{ y: -10, opacity: 0 }}
       animate={{ y: 0, opacity: 100 }}
       transition={{ ease: 'easeInOut', duration: 0.5 }}
