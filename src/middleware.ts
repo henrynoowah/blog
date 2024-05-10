c4ba54ec1d891f50a81efca103bcecbd35d70b46
조하운 <79410456+henrynoowah@users.noreply.github.com>
2024-05-11 00:54:34 +0900

    fix: updated middleware matcher to prevent matching public directory

--- src/middleware.ts
+++ src/middleware.ts
@@ -36,6 +36,6 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  matcher: '/((?!api|static|.*\\..*|_next).*)'
+  // matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 }
