f865239450043792d44c2568cfd7f06fda7640db
조하운 <79410456+henrynoowah@users.noreply.github.com>
2025-04-25 18:48:13 +0900

    chore: pages added = projects

---
+++ src/app/[locale]/projects/content-builder/page.tsx
@@ -0,0 +1,23 @@
+'use client'
+import React from 'react'
+import '@noowah/content-builder/styles.css'
+// import { EditorProvider, Canvas, Toolbar, Sidebar } from '@noowah/content-builder/editor'
+
+const Page = () => {
+  return (
+    <></>
+    // <EditorProvider
+    //   pageDetails={{
+    //     name: 'Content Builder',
+    //     slug: 'content-builder',
+    //     id: 'content-builder'
+    //   }}
+    // >
+    //   <Toolbar />
+    //   <Canvas />
+    //   <Sidebar />
+    // </EditorProvider>
+  )
+}
+
+export default Page
