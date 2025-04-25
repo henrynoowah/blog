954ad99489b330f72accf12da687da1ffc0ec45a
조하운 <79410456+henrynoowah@users.noreply.github.com>
2025-04-25 19:30:31 +0900

    chore: content-builder test

--- src/app/[locale]/projects/content-builder/page.tsx
+++ src/app/[locale]/projects/content-builder/page.tsx
@@ -1,23 +1,57 @@
 'use client'
-import React from 'react'
-// import '@noowah/content-builder/styles.css'
-// import { EditorProvider, Canvas, Toolbar, Sidebar } from '@noowah/content-builder/editor'
+
+import '@noowah/content-builder/styles.css'
+import { EditorProvider, Toolbar, useEditor } from '@noowah/content-builder/editor'
+const Canvas = dynamic(() => import('@noowah/content-builder/editor').then((mod) => mod.Canvas), { ssr: false })
+import { Sidebar } from '@noowah/content-builder/editor/plugins'
+import { defaultElementTemplates } from '@noowah/content-builder/editor/templates'
+import dynamic from 'next/dynamic'
 
 const Page = () => {
   return (
-    <></>
-    // <EditorProvider
-    //   pageDetails={{
-    //     name: 'Content Builder',
-    //     slug: 'content-builder',
-    //     id: 'content-builder'
-    //   }}
-    // >
-    //   <Toolbar />
-    //   <Canvas />
-    //   <Sidebar />
-    // </EditorProvider>
+    <>
+      <EditorProvider
+        pageDetails={{
+          name: 'Content Builder',
+          slug: 'content-builder',
+          id: 'content-builder'
+        }}
+        elementTemplates={defaultElementTemplates}
+      >
+        <></>
+        <Toolbar />
+        <ContainerElement>
+          <Canvas />
+          <Sidebar style={{ height: '100%', overflowY: 'scroll' }} />
+        </ContainerElement>
+      </EditorProvider>
+    </>
   )
 }
 
 export default Page
+
+const ContainerElement = ({ children }: { children: React.ReactNode }) => {
+  const { state } = useEditor()
+  const { liveMode } = state.editor
+  return (
+    <div
+      style={{
+        overflowY: 'auto',
+        width: '100%',
+        height: liveMode ? '100vh' : 'calc(100vh - 64px)'
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          width: '100%',
+          height: '100%',
+          alignItems: 'stretch'
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
